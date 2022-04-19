fe07a2d54ec79d94b27efad1e53721e32e9e3944
fenota-viktoriya <Fen9206@gmail.com>
2022-04-19 21:48:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './style.css';
 
 const { formRef, containerRef, btnRef } = getRefs();
-
+const apiService = new ApiService();
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-
-const apiService = new ApiService();
 
 btnRef.classList.add('is-hidden');
 
@@ -35,27 +33,13 @@ formRef.addEventListener('submit', e => {
     .then(response => {
       createGallery(response.hits);
       Notify.success(`Hooray! We found ${response.totalHits} totalHits images.`);
+      btnRef.classList.remove('is-hidden');
     })
     .catch(() => {
       btnRef.classList.add('is-hidden');
       Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     });
 });
-
-function createGallery(response) {
-  containerRef.insertAdjacentHTML('beforeend', renderMarkup(response));
-  lightbox.refresh();
-}
-
-async function axiosImg() {
-  const response = await apiService.axios().then();
-  btnRef.classList.remove('is-hidden');
-  return response;
-}
-
-function clearGallery(refs) {
-  refs.innerHTML = '';
-}
 
 btnRef.addEventListener('click', onClickLoadMoreBtn);
 
@@ -68,4 +52,18 @@ async function onClickLoadMoreBtn() {
       btnRef.classList.add('is-hidden');
       Notify.info("We're sorry, but you've reached the end of search results.");
     });
+}
+
+function createGallery(response) {
+  containerRef.insertAdjacentHTML('beforeend', renderMarkup(response));
+  lightbox.refresh();
+}
+
+async function axiosImg() {
+  const response = await apiService.axios().then();
+  return response;
+}
+
+function clearGallery(refs) {
+  refs.innerHTML = '';
 }
